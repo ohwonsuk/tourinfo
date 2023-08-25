d4d9daefb478dde3fbe240db98d48316f0c02c9e
ohwonsuk <wsoh51@gmail.com>
2023-08-26 00:45:44 +0900

    샘플 캠핑장 title, loction 데이터 생성

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const cities = require("./cities");
+const { descriptors, places } = require("./seedHelpers");
 const Campground = require("../models/campground");
 
 mongoose.connect("mongodb://127.0.0.1:27017/peter-camp", {
@@ -13,15 +14,20 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
 const seedDb = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)}, ${sample(places)}`,
     });
     await camp.save();
   }
 };
 
-seedDb();
+seedDb().then(() => {
+  mongoose.connection.close();
+});
