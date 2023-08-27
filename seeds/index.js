9b5745fe072297ff3037d115ee78fea5af3a6d42
ohwonsuk <wsoh51@gmail.com>
2023-08-27 18:31:38 +0900

    페이지별 bootstrap으로 화면 꾸미기

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,14 @@ const seedDb = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)}, ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "말 시인의 아무 아침이 까닭입니다. 별빛이 까닭이요, 마디씩 책상을 이름과 듯합니다. 별에도 봄이 둘 하나에 있습니다. 멀리 한 이름을 그리고 멀듯이, 라이너 별이 못 하나에 있습니다. 다하지 한 파란 지나고 다 풀이 위에 봅니다. 다 지나가는 이름과, 이름과, 잔디가 남은 까닭입니다. 어머니, 다하지 시인의 별 별이 이름과, 거외다.",
+      price,
     });
     await camp.save();
   }
