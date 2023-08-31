f6a30d4adc9552a5e04b9473d1bdf8b724937f34
ohwonsuk <wsoh51@gmail.com>
2023-09-01 00:53:27 +0900

    error template 화면 만들기

--- app.js
+++ app.js
@@ -97,8 +97,9 @@ app.all("*", (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = "Someting went wrong" } = err;
-  res.status(statusCode).send(message);
+  const { statusCode = 500 } = err;
+  if (!err.message) message = "Oh no, Something went wrong!";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
