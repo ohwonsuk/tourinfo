8943947c0ae7ce70937615a92966d4a8420cedca
ohwonsuk <wsoh51@gmail.com>
2023-09-06 00:01:33 +0900

    review 데이터 db 저장하기

--- models/campground.js
+++ models/campground.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// one-many 하나의 캠프에 여러개 리뷰 추가 가능 반영
+// one-many 하나의 캠핑장에 여러개 리뷰 추가 가능 반영
 // ref 로 Review 모델의 ObjectId를 참조
 
 const CampgroundSchema = new Schema({
@@ -10,7 +10,7 @@ const CampgroundSchema = new Schema({
   price: Number,
   description: String,
   location: String,
-  review: [
+  reviews: [
     {
       type: Schema.Types.ObjectId,
       ref: "Reivew",
