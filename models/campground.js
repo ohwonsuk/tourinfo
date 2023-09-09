b86953d757093b55f48aa201c1e5a5786d1738e2
ohwonsuk <wsoh51@gmail.com>
2023-09-09 17:25:35 +0900

    review 삭제하기- 개별 및 전체

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 // one-many 하나의 캠핑장에 여러개 리뷰 추가 가능 반영
@@ -16,6 +17,17 @@ const CampgroundSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+// 캠핑장 삭제시 등록된 리뷰도 같이 삭제하기 위한 미들웨어 사용
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
