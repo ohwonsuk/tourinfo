c84829f1e280becb91263e509bdfeb51d61a9245
ohwonsuk <wsoh51@gmail.com>
2023-09-13 23:16:35 +0900

    캠핑장 작성자 추가하기

--- seeds/index.js
+++ seeds/index.js
@@ -22,6 +22,7 @@ const seedDb = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "64fedca2da777d13091b78e1",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)}, ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
