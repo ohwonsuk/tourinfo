f9c7114d38ecbfafeda9fa7a21dd5a52a91d930b
ohwonsuk <wsoh51@gmail.com>
2023-09-23 00:25:55 +0900

    multer 사용 저장할 이미지 파일 정보 파싱하기

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -8,16 +8,22 @@ const {
 const campgrounds = require("../controllers/campgrounds.js");
 const Campground = require("../models/campground");
 const catchAsync = require("../utils/catchAsync");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 // route를 이용해 동일 경로의 method를 묶을 수 있음
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+    res.send("It worked !!!");
+  });
+// .post(
+//   isLoggedIn,
+//   validateCampground,
+//   catchAsync(campgrounds.createCampground)
+// );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
