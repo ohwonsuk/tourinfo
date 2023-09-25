6482b961d6ae001ca8f0506b848b6ca5c0cd5ce5
ohwonsuk <wsoh51@gmail.com>
2023-09-26 00:36:14 +0900

    이미지 미등록시 샘플이미지적용, 지도에 팝업 텍스트 적용

--- seeds/index.js
+++ seeds/index.js
@@ -22,20 +22,22 @@ const seedDb = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      // your user id
       author: "64fedca2da777d13091b78e1",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)}, ${sample(places)}`,
       description:
         "말 시인의 아무 아침이 까닭입니다. 별빛이 까닭이요, 마디씩 책상을 이름과 듯합니다. 별에도 봄이 둘 하나에 있습니다. 멀리 한 이름을 그리고 멀듯이, 라이너 별이 못 하나에 있습니다. 다하지 한 파란 지나고 다 풀이 위에 봅니다. 다 지나가는 이름과, 이름과, 잔디가 남은 까닭입니다. 어머니, 다하지 시인의 별 별이 이름과, 거외다.",
       price,
+      geometry: { type: "Point", coordinates: [128.587267, 35.861617] },
       images: [
         {
-          url: "https://res.cloudinary.com/dc2gmdv7u/image/upload/v1695454433/PeterCamp/vpqtukinljus9no7fqcv.jpg",
-          filename: "PeterCamp/vpqtukinljus9no7fqcv",
+          url: "https://res.cloudinary.com/dc2gmdv7u/image/upload/v1695654534/PeterCamp/sqnmilr50vyebvthvxmm.jpg",
+          filename: "PeterCamp/sqnmilr50vyebvthvxmm",
         },
         {
-          url: "https://res.cloudinary.com/dc2gmdv7u/image/upload/v1695454442/PeterCamp/nbqdyftbwes6vroqtw0h.jpg",
-          filename: "PeterCamp/nbqdyftbwes6vroqtw0h",
+          url: "https://res.cloudinary.com/dc2gmdv7u/image/upload/v1695654536/PeterCamp/qvvjigxibrgthrgsdwo0.jpg",
+          filename: "PeterCamp/qvvjigxibrgthrgsdwo0",
         },
       ],
     });
