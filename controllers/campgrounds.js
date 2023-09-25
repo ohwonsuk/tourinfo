53a55d187287a49835bc41f1f30b576138820c4e
ohwonsuk <wsoh51@gmail.com>
2023-09-25 22:37:50 +0900

    mapbox 가입 및 location data 저장하기

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,7 @@
 const Campground = require("../models/campground");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
@@ -13,7 +16,14 @@ module.exports.renderNewForm = (req, res) => {
 module.exports.createCampground = async (req, res) => {
   if (!req.body.campground)
     throw new ExpressError("Invalid Campground Data", 400);
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
   const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.body.features[0].geometry;
   campground.images = req.files.map((f) => ({
     url: f.path,
     filename: f.filename,
