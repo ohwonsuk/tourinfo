9927ab96414b0e9bad1ae17cf725e5c431b70c0a
ohwonsuk <wsoh51@gmail.com>
2023-09-26 11:07:47 +0900

    mapbox 지도 한국어 적용하기

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -13,3 +13,12 @@ new mapboxgl.Marker()
     )
   )
   .addTo(map);
+
+mapboxgl.setRTLTextPlugin(
+  "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js"
+);
+map.addControl(
+  new MapboxLanguage({
+    defaultLanguage: "ko",
+  })
+);
